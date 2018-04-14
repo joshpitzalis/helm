456d51aaaf4387adb089c4c8705efe3305947340
Joshpitzalis <joshpitzalis@gmail.com>
2018-04-14 11:45:03 +0530

    added puppeteer

--- node_modules/grpc/deps/grpc/src/core/lib/iomgr/resolve_address.h
+++ node_modules/grpc/deps/grpc/src/core/lib/iomgr/resolve_address.h
@@ -25,6 +25,10 @@
 
 #define GRPC_MAX_SOCKADDR_SIZE 128
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct {
   char addr[GRPC_MAX_SOCKADDR_SIZE];
   size_t len;
@@ -38,7 +42,8 @@ typedef struct {
 /* Asynchronously resolve addr. Use default_port if a port isn't designated
    in addr, otherwise use the port in addr. */
 /* TODO(ctiller): add a timeout here */
-extern void (*grpc_resolve_address)(const char* addr, const char* default_port,
+extern void (*grpc_resolve_address)(grpc_exec_ctx* exec_ctx, const char* addr,
+                                    const char* default_port,
                                     grpc_pollset_set* interested_parties,
                                     grpc_closure* on_done,
                                     grpc_resolved_addresses** addresses);
@@ -50,5 +55,9 @@ void grpc_resolved_addresses_destroy(grpc_resolved_addresses* addresses);
 extern grpc_error* (*grpc_blocking_resolve_address)(
     const char* name, const char* default_port,
     grpc_resolved_addresses** addresses);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* GRPC_CORE_LIB_IOMGR_RESOLVE_ADDRESS_H */

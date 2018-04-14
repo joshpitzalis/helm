8c4789b7aaca201c851e2aed0d813170e75e33ef
Joshpitzalis <joshpitzalis@gmail.com>
2018-04-14 11:18:20 +0530

    removed cypress tests

---
+++ src/__test__/App.test.js
@@ -0,0 +1,44 @@
+/**
+ * @jest-environment node
+ */
+
+const puppeteer = require('puppeteer');
+
+const isDebugging = () => {
+  const debugging_mode = {
+    headless: false,
+    slowMo: 250,
+    devtools: true,
+  };
+  return process.env.NODE_ENV === 'debug' ? debugging_mode : {};
+};
+
+let browser;
+let page;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({});
+  page = await browser.newPage();
+  await page.goto('http://localhost:3000/fun');
+  page.viewport({
+    width: 500,
+    height: 2400,
+  });
+});
+
+describe('on page load', () => {
+  test(
+    'h1 loads correctly',
+    async () => {
+      const html = await page.$eval('[data-test="title"]', e => e.innerHTML);
+      expect(html).toBe('Quick &amp; Easy <br> Online Polls.');
+    },
+    16000,
+  );
+});
+
+afterAll(() => {
+  if (isDebugging()) {
+    browser.close();
+  }
+});

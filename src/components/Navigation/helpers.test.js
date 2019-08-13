edf549a21298ebe149507f678e3d542bb7f69ac9
joshpitzalis <joshpitzalis@gmail.com>
2019-08-13 13:18:13 +0530

    wiped clean and restart

--- src/components/Navigation/helpers.test.js
+++
@@ -1,3 +0,0 @@
-test('clearAllNotifications clears all notifications', () => {});
-test('markNotificationAsSeen marks a notification seen', () => {});
-test('markResultsAsSeen marks a result seen', () => {});
--- src/components/Poll/__tests__/pollResults.test.js
+++
@@ -1,2 +0,0 @@
-test('poll results only show end poll early if poll is not finished yet', () => {});
-test('poll results only show delete if poll is complete', () => {});
